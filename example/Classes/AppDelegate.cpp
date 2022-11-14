de6e5cc44fefb819654991e73f956366956ea600
Luke Gehorsam <luke@heroiclabs.com>
2022-11-14 17:07:09 -0500

    fix setting of framework for osx

--- example/Classes/AppDelegate.cpp
+++ example/Classes/AppDelegate.cpp
@@ -70,9 +70,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("NakamaCocos2d", cocos2d::Rect(0, 0, 800, 600));
+        glview = GLViewImpl::createWithRect("NakamaCocos2DExample", cocos2d::Rect(0, 0, 800, 600));
 #else
-        glview = GLViewImpl::create("NakamaCocos2d");
+        glview = GLViewImpl::create("NakamaCocos2DExample");
 #endif
         director->setOpenGLView(glview);
     }
