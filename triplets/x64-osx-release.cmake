f0b4b5aa68b4884686f43ff14df9ae983b322c55
Luke Gehorsam <luke@heroiclabs.com>
2022-11-17 09:02:04 -0500

    build nakama shared but rest static

--- triplets/x64-osx-release.cmake
+++ triplets/x64-osx-release.cmake
@@ -1,6 +1,13 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+
+if (PORT MATCHES "nakama")
+    ### bring in Nakama as a shared library.
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
 set(VCPKG_BUILD_TYPE release)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
