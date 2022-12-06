e1e6b5e2402ee4621ec376e63cfe09ed7104c623
Luke Gehorsam <luke@heroiclabs.com>
2022-12-06 12:59:58 -0500

    revert back to the fetchcontent approach

--- triplets/x64-iphonesimulator-release.cmake
+++ triplets/x64-iphonesimulator-release.cmake
@@ -1,7 +1,11 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 
-set(VCPKG_LIBRARY_LINKAGE static)
+if (${PORT} MATCHES "nakama-sdk")
+    set(VCPKG_LIBRARY_LINKAGE dynamic) ## nakama can only be built as a shared library
+else()
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
 
