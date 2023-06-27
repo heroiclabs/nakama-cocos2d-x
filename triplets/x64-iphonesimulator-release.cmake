e26ec343e8eb53b08aeae1e077a06498fcef7e0e
Luke Gehorsam <luke@heroiclabs.com>
2023-06-27 16:38:47 -0700

    update to nakama-2.8.0

--- triplets/x64-iphonesimulator-release.cmake
+++ triplets/x64-iphonesimulator-release.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 
 if (${PORT} MATCHES "nakama-sdk")
-    set(VCPKG_LIBRARY_LINKAGE dynamic) ## nakama can only be built as a shared library
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
