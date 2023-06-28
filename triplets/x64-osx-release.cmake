df9e8e1a49f6f79688615d445fb5fa92cd33e60c
Luke Gehorsam <luke@heroiclabs.com>
2023-06-27 17:01:34 -0700

    fix osx release triplet

--- triplets/x64-osx-release.cmake
+++ triplets/x64-osx-release.cmake
@@ -3,7 +3,11 @@ set(VCPKG_CRT_LINKAGE dynamic)
 
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_BUILD_TYPE release)
+if (${PORT} MATCHES "nakama-sdk")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
